befe93a4c5cf5359291f344528bc4599085cd483
Dami <damilarelamine@gmail.com>
2023-12-27 16:48:28 +0100

    chore: setup user and task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, UserModule, TaskModule],
   controllers: [AppController],
   providers: [],
 })
