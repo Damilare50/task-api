7b5e47e874ca178d0c007f0e5d6850ccd7409927
Dami <damilarelamine@gmail.com>
2023-12-28 15:46:45 +0100

    chore: setup node-config-ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { validationPipeFactory } from './general/factory';
+import { config } from 'node-config-ts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(config.PORT);
 }
 bootstrap();
