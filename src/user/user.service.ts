870b92bf370ba8c9138ccca8cfbff59f4f23f714
Dami <damilarelamine@gmail.com>
2023-12-28 08:28:21 +0100

    chore: create user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { ICreateUser, IUser } from './interface';
+import { Prisma, User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: ICreateUser): Promise<IUser> {
+    const userExists = await this.prismaService.user.findFirst({
+      where: { email: data.email },
+    });
+
+    if (userExists) {
+      throw new BadRequestException(
+        `User with email ${data.email} already exists.`,
+      );
+    }
+    const user = await this.prismaService.user.create({ data });
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+  }
 }
