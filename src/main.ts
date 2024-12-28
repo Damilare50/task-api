d65f739152bea803fb82f723af50fcf185839527
Dami <damilarelamine@gmail.com>
2024-12-28 10:38:05 +0100

    chore: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { validationPipeFactory as exceptionFactory } from './general/factory';
 import { config } from 'node-config-ts';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,23 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Task Management')
+    .setDescription('Task Management API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'Authorization',
+    )
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
 
   await app.listen(config.PORT);
 }
